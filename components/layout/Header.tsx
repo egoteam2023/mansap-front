da33b7c6160c48b90ad8d67fdb8b1cd84a2c0b98
Daniyar <drtya123@mail.ru>
2024-10-30 14:16:50 +0600

    create header

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,4 +1,24 @@
-import React from 'react'
+import Link from 'next/link'
+import Button from '../ui/button'
+
 export function Header() {
-  return <section>Header</section>
+  return (
+    <header className="container flex justify-between py-4">
+      <div className="text-xl font-bold cursor-default text-slate-200">Logo</div>
+      <div className="flex items-center gap-10">
+        <Link className="text-sm text-slate-200 hover:text-slate-200/80 duration-300" href="#about">
+          О нас
+        </Link>
+        <Link className="text-sm text-slate-200 hover:text-slate-200/80 duration-300" href="#benifits">
+          Преимущества
+        </Link>
+        <Link className="text-sm text-slate-200 hover:text-slate-200/80 duration-300" href="#creators">
+          О создателях
+        </Link>
+        <Link href="#feedback">
+          <Button>Контакты</Button>
+        </Link>
+      </div>
+    </header>
+  )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,34 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+      },
+      container: {
+        center: true,
+        padding: '1rem',
+      },
+      fontSize: {
+        xs: '11px',
+        sm: '14px',
+        base: '16px',
+        lg: '18px',
+        md: '20px',
+        xl: '24px',
+        '2xl': '28px',
+        '3xl': '30px',
+        '4xl': '32px',
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
