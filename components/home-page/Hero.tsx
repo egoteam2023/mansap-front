f0151ac4ba5703afdba6857a6e208172cacda58f
Beks <bkbekmurzaev@gmail.com>
2024-10-30 21:52:48 +0600

    add Logo

--- components/home-page/Hero.tsx
+++ components/home-page/Hero.tsx
@@ -2,9 +2,10 @@ import Logo from '@/assets/logo.svg'
 import Image from 'next/image'
 export function Hero() {
   return (
-    <section className="w-full h-screen bg-black">
-      <div className="container flex justify-center items-center h-hero-sm lg:h-hero-md xl:h-hero-lg">
-        <Image width={460} height={250} src={Logo.src} alt="logo" />
+    <section className="w-full h-screen ">
+      <div className="container flex flex-col gap-5 justify-center items-center h-hero-sm lg:h-hero-md xl:h-hero-lg">
+        <Image width={500} height={250} src={Logo.src} alt="logo" />
+        <p className='text-white'>Наш слоганНаш слоганНаш слоганНаш слоган</p>
       </div>
     </section>
   )
