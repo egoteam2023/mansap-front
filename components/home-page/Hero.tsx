7ab265535ca39cf7040de418613724a371105f21
Daniyar <drtya123@mail.ru>
2024-11-01 21:01:22 +0600

    change data

--- components/home-page/Hero.tsx
+++ components/home-page/Hero.tsx
@@ -2,11 +2,11 @@ import Logo from '@/assets/logo.svg'
 import Image from 'next/image'
 export function Hero() {
   return (
-    <section id="hero" className="w-full h-screen ">
+    <section id="hero" className="w-full h-screen relative ">
       <div className="container flex flex-col gap-5 justify-center items-center h-hero-sm lg:h-hero-md xl:h-hero-lg">
         <Image width={500} height={250} src={Logo.src} alt="logo" />
-        <p className="text-slate-300">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, vero?
+        <p className="text-slate-300 absolute bottom-20">
+          Строим будущее, создавая комфорт сегодня
         </p>
       </div>
     </section>
